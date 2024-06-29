d72b398dd946e7ae13f4903bc4333adac53811bc
naimur-reza <naimur.rezaa@gmail.com>
2024-06-29 21:19:21 +0600

    Add get single data using graphql

---
+++ src/db.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: "1",
+    name: "Nike Air Jordan",
+    price: 100,
+    quantity: 3,
+  },
+  {
+    id: "2",
+    name: "Adidas Yeezy",
+    price: 150,
+    quantity: 5,
+  },
+  {
+    id: "3",
+    name: "Puma RS",
+    price: 80,
+    quantity: 2,
+  },
+  {
+    id: "4",
+    name: "Reebok Classic",
+    price: 70,
+    quantity: 1,
+  },
+];
